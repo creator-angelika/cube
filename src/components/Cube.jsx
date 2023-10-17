eeccfe2fcf6db3627dd864cb6cf9ab943f98c3f5
Angel <angelikathomas26@gmail.com>
2023-10-17 21:56:44 +0530

    second commit

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -3,7 +3,7 @@ import { useGLTF, useTexture } from '@react-three/drei';
 import * as THREE from "three";
 
 export function Cube(props) {
-  const { nodes, materials } = useGLTF('models/cube.gltf')
+  const { nodes, materials } = useGLTF('models/cube.glb')
   const texture = useTexture("textures/bake.jpg");
   const textureMaterial = new THREE.MeshStandardMaterial({
     map: texture,
@@ -11,9 +11,22 @@ export function Cube(props) {
   texture.flipY=false;
   return (
     <group {...props} dispose={null}>
-      <mesh name="Cube001" geometry={nodes.Cube001.geometry} material={textureMaterial} position={[0, 1, -3]} />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube.geometry}
+        material={textureMaterial}
+        position={[0, 1, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube001.geometry}
+        material={textureMaterial}
+        position={[0, 1, -3]}
+      />
     </group>
   )
 }
 
-useGLTF.preload('models/cube.gltf')
+useGLTF.preload('models/cube.glb')
